3cbb3782d88176b5aa6a80de9ee77db306ee3026
Vadim Askarov <93433088+shiro-kreishi@users.noreply.github.com>
2024-03-27 19:28:42 +0500

    added the ability to interact with objects

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -7,10 +7,12 @@ public class ActivateTeleportationRay : MonoBehaviour
     public GameObject rightTeleportation;
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
     
     void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
