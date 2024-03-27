24be3adf84eddb41e2bfc59ac24e953635e230fb
Vadim Askarov <93433088+shiro-kreishi@users.noreply.github.com>
2024-03-27 18:21:29 +0500

    add player movement via teleportation

---
+++ Assets/ActivateTeleportationRay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportation;
+    public GameObject rightTeleportation;
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+    
+    void Update()
+    {
+        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
