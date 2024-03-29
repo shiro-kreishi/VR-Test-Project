8851071668f6686db61b396997a984bf5dc2585c
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2024-03-29 16:32:50 +0500

    make grabbing MP

--- Assets/Scripts/VrRigPreference.cs
+++ Assets/Scripts/VrRigPreference.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class VrRigPreference : MonoBehaviour
 {
     public static VrRigPreference Singleton;
-    
+
+    public GameObject XRRig;
     public Transform root;
     public Transform head;
     public Transform leftHand;
---
+++ Assets/Scripts/XRGrabEventHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabEventHandler : MonoBehaviour
+{
+    public ulong avatarNetworkObjectId;
+    public GameObject avatarObject;
+
+    public void SelectLeftGrabEtnerEventController(SelectEnterEventArgs eventArgs) {
+        var grabbedObj = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+        avatarObject.GetComponent<NetworkPlayer>().AvatarSelectGrabEnterEventHub(grabbedObj, true);
+    }
+
+    public void SelectLeftGrabExitEventController(SelectExitEventArgs eventArgs) {
+        var grabbedObj = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+        avatarObject.GetComponent<NetworkPlayer>().AvatarSelectGrabExitEventHub(grabbedObj, true);
+    }
+    public void SelectRightGrabEtnerEventController(SelectEnterEventArgs eventArgs) {
+        var grabbedObj = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+        avatarObject.GetComponent<NetworkPlayer>().AvatarSelectGrabEnterEventHub(grabbedObj, false);
+    }
+
+    public void SelectRightGrabExitEventController(SelectExitEventArgs eventArgs) {
+        var grabbedObj = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+        avatarObject.GetComponent<NetworkPlayer>().AvatarSelectGrabExitEventHub(grabbedObj, false);
+    }
+}
